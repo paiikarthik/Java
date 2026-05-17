0e88f80b79087e343e184cdb17b08c5efeb23cf7
Karthik <karthikpai282@gmail.com>
2026-05-17 23:00:23 +0530

    Implement lengthOfLastWord method in Solution class

---
+++ OOP/lengthoflastword.java
@@ -0,0 +1,21 @@
+//to count length of last word - Leet code -main function
+class Solution {
+    public int lengthOfLastWord(String s) {
+         int n = s.length();
+         int count = 0;
+
+         for(int i = n - 1; i >= 0; i--) {
+             char ch = s.charAt(i);
+
+             if(ch == ' ' && count > 0) {
+                 break;
+             }
+
+             if(ch != ' ') {
+                 count = count + 1;
+             }
+         }
+
+         return count;
+    }
+}
