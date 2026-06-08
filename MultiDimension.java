cb9a52b109a39928a96d349010caa02aa425434b
Karthik <karthikpai282@gmail.com>
2026-06-08 16:26:15 +0530

    Add files via upload

---
+++ MultiDimension.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class MultiDimension {
+   public static void main(String[] args) {
+	int [][] ar= {{11,12,13,14},{13,14,15,15}};
+	
+	for (int i=0;i<2;i++) {
+		for(int j=0;j<4;j++) {
+			System.out.print(ar[i][j]+ " ");
+	
+			
+		}	System.out.println();
+	}
+}
+}
+
+
+//jagged array
+//ar.length
+//ar[r].length
