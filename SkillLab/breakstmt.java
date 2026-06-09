81441e5796d48907aacc4a7dfcc5519bf97c902d
Karthik <karthikpai282@gmail.com>
2026-06-09 15:29:07 +0530

    Add breakstmt class with invoke method

---
+++ SkillLab/breakstmt.java
@@ -0,0 +1,21 @@
+package SkillLab;
+
+public class breakstmt {
+  static void invoke() { 
+	 for(int i=1;i<=10;i++)
+	 {
+		 if(i==5)
+			// continue;
+			 // break;
+			//System.exit(0);
+			 return;
+		 else
+		   System.out.println(i);
+	 }
+  }
+  public static void main(String[] args) {
+	 
+	  invoke();
+	
+}
+}
