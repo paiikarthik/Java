c2e88e578402ee4f46f185512bbd0fa624a3d747
Karthik <karthikpai282@gmail.com>
2026-06-11 12:54:51 +0530

    Add files via upload

--- SkillLab/Work/EmployeeMain.java
+++ SkillLab/Work/EmployeeMain.java
@@ -1,137 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package Day3Pack;
+
 import java.util.Scanner;
 
-class EmpNoGenerator
-{
-	static int emplId;
-	static
-	{
-		emplId=0;
+public class EmployeeMain {
+	public static void main(String[] args) {
+		Employee []arr  = new Employee[100];
+		int count=0;
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		String str;
+		do
+		{
+			System.out.println("1:Add Employee 2:Display Employee 3:Search By Designattion 4:Search By ID 5:Exit");
+			System.out.println("Enter your choice:");
+			choice= sc.nextInt();
+			switch(choice)
+			{
+			case 1: 
+				if(count<arr.length)
+				{
+					System.out.println("Enter the name");
+					String name = sc.next();
+					System.out.println("Enter the Salary:");
+					double sal = sc.nextDouble();
+					
+					System.out.println("Enter the Designation:");
+					String desig = sc.next(); 
+			        System.out.println("Enter the Organization Name:");
+			        String org=sc.next();
+					arr[count] = new Employee(empIDGenerator.getID(),name , sal, desig,org);
+					count++;
+				}
+				else
+					System.out.println("No more employees can be added");
+				break;
+				
+			case 2:
+				if(count==0)
+					System.out.println("Employee records are not yet available");
+				else
+					for(int i=0;i<count;i++)
+					   arr[i].dispEmpDetails();
+				break;
+				
+			case 3:
+				if(count==0)
+					System.out.println("Employee records are not yet available");
+				else
+				{
+					System.out.println("enter the designation");
+					String desig = sc.next();
+					Employee.searchEmpRecord(desig, arr);
+				}
+				break;
+			case 4:
+				if(count==0)
+					System.out.println("Emp records are not yet available");
+				else
+				{
+					System.out.println("enter the id");
+					int id = sc.nextInt();
+					Employee.searchEmpRecord(id, arr);
+				}
+				break;
+			case 5:
+				System.out.println("Exiting....");
+				System.exit(0);
+				
+			default:
+				System.out.println("Invalid choice");
+				break;
+				
+			}
+		
+		}while(true);
+		
 	}
-	static int getEmpNo()
-	{
-		return ++emplId;
-	}
-}
-class Employee{
-   private int empId;
-   String empName, designation, org;
-   float salary;
-   
-   
-   Employee(int emplId,String empName, String designation, String org, float salary){
-       this.empName=empName;
-       this.designation=designation;
-       this.org=org;
-       empId=emplId;
-       this.salary=salary;
-   }
-   
-    void  displayEmployeeDetails(){
-       System.out.println("Employee id:"+empId);
-       System.out.println("Employee Name:"+empName);
-       System.out.println("Employee designation:"+designation);
-       System.out.println("Employee organiztion:"+org);
-       System.out.println("Employee salary:"+salary);
-   }
-   
-  static void searchEmpRecord(String desig, Employee []arr)
-   {
-       boolean status=false;
-       for(int i=0;i<arr.length;i++){
-           if(arr[i]!=null){
-               if (arr[i].designation.equals(desig)){
-                   status=true;
-                   arr[i].displayEmployeeDetails();
-                           
-               }
-               
-           }
-       }
-       
-       if(status=false)
-           System.out.println("No emp records in thAT DESIGNATION");
-   }
-    
-   static   void searchEmpID(int ID, Employee []arr)
-   {
-       boolean status=false;
-       for(int i=0;i<arr.length;i++){
-           if(arr[i]!=null){
-               if(arr[i].empId==ID){
-                   status=true;
-                   arr[i].displayEmployeeDetails();
-                           
-               }
-               
-           }
-       }
-       
-       if(status=false)
-           System.out.println("No emp records in that ID");
-   }
-    
+
 }
 
-public class EmployeeMain {
-      public static void main(String[] main)
-      {
-          Employee [] arr= new Employee[3];
-          Scanner sc= new Scanner(System.in);
-          int count=0;
-          int choice;
-          do{
-              System.out.println("1: Add Emp 2: Dis Emp 3:Search BY Designation 4:SearchBYID");
-              System.out.println("Enter Your Choice:");
-              choice=sc.nextInt();
-              switch(choice){
-                  case 1:
-                      if (count<arr.length)
-                      {
-                            System.out.println("Enter Employee Name:");
-                            String empName=sc.next();
-              
-                            System.out.println("Enter Employee Salary:");
-                            float sal=sc.nextFloat();
-              
-                            System.out.println("Enter Employee Designation:");
-                            String desig=sc.next();
-              
-              
-                            System.out.println("Enter Employee Organization:");
-                            String org=sc.next();
-                            arr[count] = new Employee(EmpNoGenerator.getEmpNo(),empName, desig, org,sal);
-                            count++;
-                          
-                      }else{
-                          System.out.println("No More Employees can be added");
-                      } break;
-                  case 2:
-                      for(int i=0;i<count;i++)
-			 arr[i].displayEmployeeDetails();
-			break;
-                       
-                  case 3:
-                      System.out.println("Enter Employee designation");
-                      String desig = sc.next();
-		      Employee.searchEmpRecord(desig, arr);
-                      break;
-                  case 4:
-                      System.out.println("Enter Employee id");
-			 int id = sc.nextInt();
-		      Employee.searchEmpID(id, arr);
-                       break;
-               
-                  default:
-                      System.out.println("Invalid Choice");
-                       break;
-              
-              }
-          }while(true);
-      }   
-         
-      }     
+
+
+
+
+
+
 
